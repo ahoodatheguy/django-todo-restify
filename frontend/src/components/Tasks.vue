ab2431557ef9e263fb9b7e41ac85ecde7ae22ecb
Ahadu Kebede <ahadukebede@gmail.com>
2022-02-04 17:27:50 -0500

    Added ability to delete task

--- frontend/src/components/Tasks.vue
+++ frontend/src/components/Tasks.vue
@@ -1,6 +1,6 @@
 <template>
 	<div v-for="event in events" :key="event.id">
-		<h3>{{event.name}}</h3>
+		<h3 class="task-name" @click="deleteTask(event.id)">{{event.name}}</h3>
 		<p>{{event.description}}</p>
 	</div>
 </template>
@@ -12,7 +12,14 @@ export default {
 	name: 'Tasks',
 	data() {
 		return {
-		events: {}
+		events: {},
+		}
+	},
+	methods: {
+		deleteTask(id) {
+			axios
+			.delete(`http://127.0.0.1:8000/api/delete-task/${id}`)
+			.then(() => location.reload)
 		}
 	},
 	created() {
@@ -26,5 +33,9 @@ export default {
 </script>
 
 <style>
-
+.task-name:hover {
+	color: red;
+	cursor: pointer;
+	text-decoration: line-through;
+}
 </style>
