332bf68b6685f2291c396ee6c4256dbbeef62987
Ahadu Kebede <ahadukebede@gmail.com>
2022-02-04 16:00:17 -0500

    Created `Task` model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+
+class Task(models.Model):
+	name = models.CharField(max_length=50)
+	description = models.TextField()
+	date = models.DateTimeField(default=timezone.now())
