63c630dfefe64df338d05d20a8a22135ebde6033
Ahadu Kebede <ahadukebede@gmail.com>
2022-02-04 15:56:40 -0500

    added index api view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def index(request):
+	urls = {
+		'api/': 'route index',
+		'/view-tasks/': 'view current tasks',
+		'/create-task/<str:name>': 'create new task',
+		'/delete-task/<int:id>': 'delete task',
+		'/task-detail/<int:id>': 'view task details'
+	}
+	return Response(urls)
